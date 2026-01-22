530d3643540edee90c06b01ada9971ae9627374e
Sama Sameh <samasameh587@gmail.com>
2026-01-22 22:48:45 +0200

    Build Onboarding screen.

---
+++ lib/ui/screens/onboarding_screen/navigation_builder.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../../utils/app_colors.dart';
+import '../../widgets/app_constants.dart';
+
+class NavigationBuilder extends StatelessWidget {
+  final int currentIndex;
+
+  const NavigationBuilder({
+    super.key,
+    required this.currentIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        AppConstants.pages.length,
+            (index) => AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          margin: const EdgeInsets.symmetric(horizontal: 4),
+          height: 8,
+          width: currentIndex == index ? 35 : 8,
+          decoration: BoxDecoration(
+            color: currentIndex == index
+                ? AppColors.blue
+                : AppColors.darkBlue.withOpacity(0.3),
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/screens/onboarding_screen/onboarding_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import '../../utils/app_colors.dart';
+import '../../utils/app_styles.dart';
+import '../../widgets/app_constants.dart';
+import 'build_elevated_button.dart';
+import 'navigation_builder.dart';
+import 'pageview_builder.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  static const String routeName = "Onboarding";
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  late PageController _imageController;
+  late PageController _textController;
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _imageController = PageController();
+    _textController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _imageController.dispose();
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: PageviewBuilder(
+                pageController: _imageController,
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                  _textController.animateToPage(
+                    index,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Container(
+                color: AppColors.babyBlue,
+                width: double.infinity,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    NavigationBuilder(currentIndex: _currentIndex),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: PageView.builder(
+                        controller: _textController,
+                        itemCount: AppConstants.pages.length,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return Center(
+                            child: Text(
+                              AppConstants.pages[index]['desc']!,
+                              textAlign: TextAlign.center,
+                              style: AppStyles.blue50regular,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    BuildElevatedButton(
+                      currentIndex: _currentIndex,
+                      imageController: _imageController,
+                    ),
+                    if (_currentIndex ==
+                        AppConstants.pages.length - 1) ...[
+                      const SizedBox(height: 8),
+                      const Text(
+                        "ابدأ الآن",
+                        style: AppStyles.blue26regular,
+                      ),
+                    ],
+                    const SizedBox(height: 20,)
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
