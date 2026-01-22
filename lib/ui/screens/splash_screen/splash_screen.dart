6c8e53178377c1ce349e71266bfc5e08b7b9d8cb
Sama Sameh <samasameh587@gmail.com>
2026-01-22 22:47:04 +0200

    Build splash screen.

---
+++ lib/ui/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../../utils/app_assets.dart';
+import '../../utils/app_colors.dart';
+import '../../utils/app_styles.dart';
+import '../onboarding_screen/onboarding.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  static const String routeName = "splashScreen";
+
+  @override
+  State<SplashScreen> createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  void startTimer() {
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const OnboardingScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AppColors.babyBlue,
+        body: Container(
+          margin: const EdgeInsets.all(40),
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset(
+                AppAssets.fciLogo,
+                height: 180,
+                alignment: AlignmentGeometry.topLeft,
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              const Text(
+                'مرحبا بكم',
+                style: AppStyles.blue50regular,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'في منصة خدمات كلية الحاسبات والمعلومات',
+                style: AppStyles.blue26regular,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
